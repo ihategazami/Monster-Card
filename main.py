6d673f5ba8527e38e0aab05e425b77f983100acc
Lucas <k21146678@gmail.com>
2026-04-30 14:32:15 +1200

    Nested dictionary

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ STATS = ["strength", "speed", "stealth", "cunning"]
 # Catalogue setup and formatting
 # -----------------------------------------------------------------------------
 def create_catalogue():
-    """Create a small test catalogue while the data structure is being developed."""
+    """Create and return the starting nested dictionary catalogue."""
     return {
         "Stoneling": {"strength": 7, "speed": 1, "stealth": 25, "cunning": 15},
-        "Vexscream": {"strength": 1, "speed": 6, "stealth": 21, "cunning": 19}
+        "Vexscream": {"strength": 1, "speed": 6, "stealth": 21, "cunning": 19},
+        "Dawnmirage": {"strength": 5, "speed": 15, "stealth": 18, "cunning": 22},
+        "Blazegolem": {"strength": 15, "speed": 20, "stealth": 23, "cunning": 6},
+        "Websnake": {"strength": 7, "speed": 15, "stealth": 10, "cunning": 5},
+        "Moldvine": {"strength": 21, "speed": 18, "stealth": 14, "cunning": 5},
+        "Vortexwing": {"strength": 19, "speed": 13, "stealth": 19, "cunning": 2},
+        "Rotthing": {"strength": 16, "speed": 7, "stealth": 4, "cunning": 12},
+        "Froststep": {"strength": 14, "speed": 14, "stealth": 17, "cunning": 4},
+        "Wispghoul": {"strength": 17, "speed": 19, "stealth": 3, "cunning": 2}
     }
 
 # -----------------------------------------------------------------------------
